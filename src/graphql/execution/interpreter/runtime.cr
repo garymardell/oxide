8f757c6c243ea4c81e3d9715a5fc37c08d57abc9
Gary Mardell <info@garymardell.co.uk>
2019-12-06 11:32:30 -0800

    Update execute_select_set to group fields

--- src/graphql/execution/interpreter/runtime.cr
+++ src/graphql/execution/interpreter/runtime.cr
@@ -25,21 +25,24 @@ module Graphql
         def execute_selection_set(selection_set, object_type, object_value) # TODO: variable_values
           result = Hash(String, ReturnType).new
 
-          selection_set.each do |selection|
-            field_name = selection.name
+          grouped_field_set = collect_fields(object_type, selection_set)
+
+          grouped_field_set.each do |response_key, fields|
+            field_name = fields.first.name
 
             if field = object_type.get_field(field_name)
               field_type = field.type
 
-              result[field_name] =
-                execute_field(object_type, object_value, field_type, selection)
+              result[response_key] =
+                execute_field(object_type, object_value, field_type, fields)
             end
           end
 
           result.as(ReturnType)
         end
 
-        def execute_field(object_type, object_value, field_type, field)
+        def execute_field(object_type, object_value, field_type, fields)
+          field = fields.first
           field_name = field.name
 
           resolved_value = object_type.resolver.try &.resolve(object_value, field_name)
@@ -55,6 +58,26 @@ module Graphql
 
         def complete_value(field_type : Graphql::Schema::Scalar, field, result)
           result.as(ReturnType)
+        end
+
+        def collect_fields(object_type, selection_set) # TODO: variable_values, visited_fragments
+          grouped_fields = {} of String => Array(Graphql::Language::Nodes::Field)
+
+          selection_set.each do |selection|
+            # TODO: @skip directive
+            # TODO: @include directive
+            case selection
+            when Graphql::Language::Nodes::Field
+              response_key = selection.name
+
+              grouped_fields[response_key] ||= [] of Graphql::Language::Nodes::Field
+              grouped_fields[response_key] << selection
+            end
+            # TODO: fragment spread
+            # TODO: inline fragment
+          end
+
+          grouped_fields
         end
       end
     end
