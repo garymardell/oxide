bf6aa1e0002cf3b89cc59f216758bafcac41118f
Gary Mardell <info@garymardell.co.uk>
2021-10-24 21:54:21 +0100

    Complete validation schema spec helper

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -8,51 +8,6 @@ class NullResolver < Graphene::Schema::Resolver
   end
 end
 
-# type Query {
-#   dog: Dog
-# }
-
-# enum DogCommand { SIT, DOWN, HEEL }
-
-# type Dog implements Pet {
-#   name: String!
-#   nickname: String
-#   barkVolume: Int
-#   doesKnowCommand(dogCommand: DogCommand!): Boolean!
-#   isHousetrained(atOtherHomes: Boolean): Boolean!
-#   owner: Human
-# }
-
-# interface Sentient {
-#   name: String!
-# }
-
-# interface Pet {
-#   name: String!
-# }
-
-# type Alien implements Sentient {
-#   name: String!
-#   homePlanet: String
-# }
-
-# type Human implements Sentient {
-#   name: String!
-# }
-
-# enum CatCommand { JUMP }
-
-# type Cat implements Pet {
-#   name: String!
-#   nickname: String
-#   doesKnowCommand(catCommand: CatCommand!): Boolean!
-#   meowVolume: Int
-# }
-
-# union CatOrDog = Cat | Dog
-# union DogOrHuman = Dog | Human
-# union HumanOrAlien = Human | Alien
-
 DogCommandEnum = Graphene::Type::Enum.new(
   name: "DogCommand",
   values: [
@@ -64,7 +19,7 @@ DogCommandEnum = Graphene::Type::Enum.new(
 
 class PetTypeResolver < Graphene::Schema::TypeResolver
   def resolve_type(object, context)
-    DogObject
+    DogType
   end
 end
 
@@ -80,6 +35,35 @@ SentientInterface = Graphene::Type::Interface.new(
   ]
 )
 
+AlienType = Graphene::Type::Object.new(
+  name: "Alien",
+  fields: [
+    Graphene::Schema::Field.new(
+      name: "name",
+      type: Graphene::Type::NonNull.new(
+        of_type: Graphene::Type::String.new
+      )
+    ),
+    Graphene::Schema::Field.new(
+      name: "homePlanet",
+      type: Graphene::Type::String.new
+    )
+  ]
+)
+
+HumanType = Graphene::Type::Object.new(
+  name: "Human",
+  fields: [
+    Graphene::Schema::Field.new(
+      name: "name",
+      type: Graphene::Type::NonNull.new(
+        of_type: Graphene::Type::String.new
+      )
+    )
+  ]
+)
+
+
 PetInterface = Graphene::Type::Interface.new(
   name: "Pet",
   fields: [
@@ -92,9 +76,121 @@ PetInterface = Graphene::Type::Interface.new(
   ]
 )
 
-DogObject = Graphene::Type::Object.new(
+CatCommandEnum = Graphene::Type::Enum.new(
+  name: "CatCommand",
+  values: [
+    Graphene::Type::EnumValue.new(name: "JUMP")
+  ]
+)
+
+CatType = Graphene::Type::Object.new(
+  name: "Cat",
+  implements: [PetInterface],
+  fields: [
+    Graphene::Schema::Field.new(
+      name: "name",
+      type: Graphene::Type::NonNull.new(
+        of_type: Graphene::Type::String.new
+      )
+    ),
+    Graphene::Schema::Field.new(
+      name: "nickname",
+      type: Graphene::Type::String.new
+    ),
+    Graphene::Schema::Field.new(
+      name: "doesKnowCommand",
+      arguments: [
+        Graphene::Schema::Argument.new(
+          name: "catCommand",
+          type: Graphene::Type::NonNull.new(
+            of_type: CatCommandEnum
+          )
+        )
+      ],
+      type: Graphene::Type::NonNull.new(
+        of_type: Graphene::Type::Boolean.new
+      )
+    ),
+    Graphene::Schema::Field.new(
+      name: "meowVolume",
+      type: Graphene::Type::Int.new
+    )
+  ]
+)
+
+CatOrDogUnion = Graphene::Type::Union.new(
+  name: "CatOrDog",
+  possible_types: [
+    CatType.as(Graphene::Type),
+    DogType.as(Graphene::Type)
+  ]
+)
+
+DogOrHumanUnion = Graphene::Type::Union.new(
+  name: "DogOrHuman",
+  possible_types: [
+    DogType.as(Graphene::Type),
+    HumanType.as(Graphene::Type)
+  ]
+)
+
+HumanOrAlienUnion = Graphene::Type::Union.new(
+  name: "HumanOrAlien",
+  possible_types: [
+    AlienType.as(Graphene::Type),
+    HumanType.as(Graphene::Type)
+  ]
+)
+
+DogType = Graphene::Type::Object.new(
   name: "Dog",
   implements: [PetInterface],
+  fields: [
+    Graphene::Schema::Field.new(
+      name: "name",
+      type: Graphene::Type::NonNull.new(
+        of_type: Graphene::Type::String.new
+      )
+    ),
+    Graphene::Schema::Field.new(
+      name: "nickname",
+      type: Graphene::Type::String.new
+    ),
+    Graphene::Schema::Field.new(
+      name: "barkVolume",
+      type: Graphene::Type::Int.new
+    ),
+    Graphene::Schema::Field.new(
+      name: "doesKnowCommand",
+      arguments: [
+        Graphene::Schema::Argument.new(
+          name: "dogCommand",
+          type: Graphene::Type::NonNull.new(
+            of_type: DogCommandEnum
+          )
+        )
+      ],
+      type: Graphene::Type::NonNull.new(
+        of_type: Graphene::Type::Boolean.new
+      )
+    ),
+    Graphene::Schema::Field.new(
+      name: "isHousetrained",
+      arguments: [
+        Graphene::Schema::Argument.new(
+          name: "atOtherHomes",
+          type: Graphene::Type::Boolean.new
+        )
+      ],
+      type: Graphene::Type::NonNull.new(
+        of_type: Graphene::Type::Boolean.new
+      )
+    ),
+    Graphene::Schema::Field.new(
+      name: "owner",
+      type: HumanType
+    )
+  ]
 )
 
 ValidationsSchema = Graphene::Schema.new(
@@ -103,8 +199,13 @@ ValidationsSchema = Graphene::Schema.new(
     fields: [
       Graphene::Schema::Field.new(
         name: "dog",
-        type: DogObject
+        type: DogType
       )
     ]
-  )
+  ),
+  orphan_types: [
+    CatOrDogUnion.as(Graphene::Type),
+    DogOrHumanUnion.as(Graphene::Type),
+    HumanOrAlienUnion.as(Graphene::Type)
+  ]
 )
