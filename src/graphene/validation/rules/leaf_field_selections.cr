68933c3a6094f7895d22d36a8ac01cbb6f150801
Gary Mardell <info@garymardell.co.uk>
2021-10-24 22:27:27 +0100

    Validate leaf field selections

---
+++ src/graphene/validation/rules/leaf_field_selections.cr
@@ -0,0 +1,26 @@
+# For each selection in the document
+# Let selectionType be the result type of selection
+# If selectionType is a scalar or enum:
+#   The subselection set of that selection must be empty
+# If selectionType is an interface, union, or object
+#   The subselection set of that selection must NOT BE empty
+module Graphene
+  module Validation
+    class LeafFieldSelections < Rule
+      def enter(node : Graphene::Language::Nodes::Field, context)
+        selection_type = context.field_definition.try &.type
+
+        case selection_type
+        when Graphene::Type::Scalar, Graphene::Type::Enum
+          if node.selection_set && node.selection_set.not_nil!.selections.any?
+            errors << Error.new("selections on scalar values are not allowed")
+          end
+        when Graphene::Type::Interface, Graphene::Type::Union, Graphene::Type::Object
+          if node.selection_set.nil? || node.selection_set.not_nil!.selections.empty?
+            errors << Error.new("selection must be provided for #{node.name}")
+          end
+        end
+      end
+    end
+  end
+end
