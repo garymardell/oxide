1cdcfe84ede9d92922ab18d0fa6b9659e63ed47e
Gary Mardell <info@garymardell.co.uk>
2023-06-01 09:10:06 -0700

    Update examples for field selections

--- spec/graphene/validation/field_selections_spec.cr
+++ spec/graphene/validation/field_selections_spec.cr
@@ -1,125 +1,118 @@
 require "../../spec_helper"
 
 describe Graphene::Validation::FieldSelections do
-  describe "objects" do
-    it "gives an error if an selection does not exist" do
-      query_string = <<-QUERY
-        {
-          dog(name: "George") {
-            meowVolume
-          }
-        }
-      QUERY
+  it "counter example #115" do
+    query_string = <<-QUERY
+      fragment fieldNotDefined on Dog {
+        meowVolume
+      }
 
-      query = Graphene::Query.new(query_string)
+      fragment aliasedLyingFieldTargetNotDefined on Dog {
+        barkVolume: kawVolume
+      }
+    QUERY
 
-      pipeline = Graphene::Validation::Pipeline.new(
-        ValidationsSchema,
-        query,
-        [Graphene::Validation::FieldSelections.new.as(Graphene::Validation::Rule)]
-      )
+    query = Graphene::Query.new(query_string)
 
-      pipeline.execute
+    pipeline = Graphene::Validation::Pipeline.new(
+      ValidationsSchema,
+      query,
+      [Graphene::Validation::FieldSelections.new.as(Graphene::Validation::Rule)]
+    )
 
-      pipeline.errors.size.should eq(1)
-      pipeline.errors.should contain(Graphene::Error.new("Field \"meowVolume\" does not exist on \"Dog\""))
-    end
+    pipeline.execute
 
-    it "gives an error when field does not exist when aliased to a field that does exist" do
-      query_string = <<-QUERY
-        {
-          dog(name: "George") {
-            barkVolume: kawVolume
-          }
-        }
-      QUERY
-
-      query = Graphene::Query.new(query_string)
-
-      pipeline = Graphene::Validation::Pipeline.new(
-        ValidationsSchema,
-        query,
-        [Graphene::Validation::FieldSelections.new.as(Graphene::Validation::Rule)]
-      )
-
-      pipeline.execute
-
-      pipeline.errors.size.should eq(1)
-      pipeline.errors.should contain(Graphene::Error.new("Field \"kawVolume\" does not exist on \"Dog\""))
-    end
+    pipeline.errors.size.should eq(2)
+    pipeline.errors.should contain(Graphene::Error.new("Field \"meowVolume\" does not exist on \"Dog\""))
+    pipeline.errors.should contain(Graphene::Error.new("Field \"kawVolume\" does not exist on \"Dog\""))
   end
 
-  describe "interfaces" do
-    it "gives an error if an selection does not exist" do
-      query_string = <<-QUERY
-        fragment definedOnImplementorsButNotInterface on Pet {
-          nickname
-        }
-      QUERY
+  it "example #116" do
+    query_string = <<-QUERY
+      fragment interfaceFieldSelection on Pet {
+        name
+      }
+    QUERY
 
-      query = Graphene::Query.new(query_string)
+    query = Graphene::Query.new(query_string)
 
-      pipeline = Graphene::Validation::Pipeline.new(
-        ValidationsSchema,
-        query,
-        [Graphene::Validation::FieldSelections.new.as(Graphene::Validation::Rule)]
-      )
+    pipeline = Graphene::Validation::Pipeline.new(
+      ValidationsSchema,
+      query,
+      [Graphene::Validation::FieldSelections.new.as(Graphene::Validation::Rule)]
+    )
 
-      pipeline.execute
+    pipeline.execute
 
-      pipeline.errors.size.should eq(1)
-      pipeline.errors.should contain(Graphene::Error.new("Field \"nickname\" does not exist on \"Pet\""))
-    end
+    pipeline.errors.size.should eq(0)
   end
 
-  describe "unions" do
-    it "fields may not be selected directly from union" do
-      query_string = <<-QUERY
-        fragment directFieldSelectionOnUnion on CatOrDog {
+  it "counter example #117" do
+    query_string = <<-QUERY
+      fragment definedOnImplementorsButNotInterface on Pet {
+        nickname
+      }
+    QUERY
+
+    query = Graphene::Query.new(query_string)
+
+    pipeline = Graphene::Validation::Pipeline.new(
+      ValidationsSchema,
+      query,
+      [Graphene::Validation::FieldSelections.new.as(Graphene::Validation::Rule)]
+    )
+
+    pipeline.execute
+
+    pipeline.errors.size.should eq(1)
+    pipeline.errors.should contain(Graphene::Error.new("Field \"nickname\" does not exist on \"Pet\""))
+  end
+
+  it "example #118" do
+    query_string = <<-QUERY
+      fragment inDirectFieldSelectionOnUnion on CatOrDog {
+        __typename
+        ... on Pet {
           name
+        }
+        ... on Dog {
           barkVolume
         }
-      QUERY
+      }
+    QUERY
 
-      query = Graphene::Query.new(query_string)
+    query = Graphene::Query.new(query_string)
 
-      pipeline = Graphene::Validation::Pipeline.new(
-        ValidationsSchema,
-        query,
-        [Graphene::Validation::FieldSelections.new.as(Graphene::Validation::Rule)]
-      )
+    pipeline = Graphene::Validation::Pipeline.new(
+      ValidationsSchema,
+      query,
+      [Graphene::Validation::FieldSelections.new.as(Graphene::Validation::Rule)]
+    )
 
-      pipeline.execute
+    pipeline.execute
 
-      pipeline.errors.size.should eq(2)
-      pipeline.errors.should contain(Graphene::Error.new("Field \"name\" can not be selected on union type \"CatOrDog\""))
-    end
+    pipeline.errors.size.should eq(0)
+  end
 
-    it "__typename field can be selected" do
-      query_string = <<-QUERY
-        fragment directFieldSelectionOnUnion on CatOrDog {
-          __typename
+  it "counter example #119" do
+    query_string = <<-QUERY
+      fragment directFieldSelectionOnUnion on CatOrDog {
+        name
+        barkVolume
+      }
+    QUERY
 
-          ... on Pet {
-            name
-          }
-          ... on Dog {
-            barkVolume
-          }
-        }
-      QUERY
+    query = Graphene::Query.new(query_string)
 
-      query = Graphene::Query.new(query_string)
+    pipeline = Graphene::Validation::Pipeline.new(
+      ValidationsSchema,
+      query,
+      [Graphene::Validation::FieldSelections.new.as(Graphene::Validation::Rule)]
+    )
 
-      pipeline = Graphene::Validation::Pipeline.new(
-        ValidationsSchema,
-        query,
-        [Graphene::Validation::FieldSelections.new.as(Graphene::Validation::Rule)]
-      )
+    pipeline.execute
 
-      pipeline.execute
-
-      pipeline.errors.size.should eq(0)
-    end
+    pipeline.errors.size.should eq(2)
+    pipeline.errors.should contain(Graphene::Error.new("Field \"name\" can not be selected on union type \"CatOrDog\""))
   end
 end
