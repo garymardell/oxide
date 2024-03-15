21a86a352ec0a55305672a3fbe351bfaadaec63b
Gary Mardell <info@garymardell.co.uk>
2024-03-15 15:53:30 -0700

    Allow Oxide::Query to be JSON serializable

--- src/oxide/query.cr
+++ src/oxide/query.cr
@@ -1,23 +1,31 @@
 module Oxide
   class Query
+    include JSON::Serializable
     include Language::Visitable
 
+    @[JSON::Field(key: "query")]
     getter query_string : String
-    getter context : Oxide::Context?
     getter variables : Hash(String, JSON::Any)
-    getter operation_name : String | Nil
-    getter document : Oxide::Language::Nodes::Document
 
-    def initialize(@query_string, @context = nil, @variables = {} of String => JSON::Any, @operation_name = nil)
-      @document = parse(@query_string)
+    @[JSON::Field(key: "operationName")]
+    getter operation_name : String?
+
+    @[JSON::Field(ignore: true)]
+    @document : Oxide::Language::Nodes::Document?
+
+    def initialize(@query_string, @variables = {} of String => JSON::Any, @operation_name = nil)
     end
 
     def accept(visitor : Language::Visitor)
       document.accept(visitor)
     end
 
+    def document : Oxide::Language::Nodes::Document
+      @document ||= parse(query_string)
+    end
+
     private def parse(query_string)
-      Oxide::Language::Parser.parse(query_string).as(Oxide::Language::Nodes::Document)
+      Oxide::Language::Parser.parse(query_string)
     end
   end
 end
