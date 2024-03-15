9ec44313d212b628ef5bfe94a6e4ef206dc80152
Gary Mardell <info@garymardell.co.uk>
2024-03-15 10:27:24 -0700

    Expect values to be JSON::Any structs

--- src/oxide/applied_directive.cr
+++ src/oxide/applied_directive.cr
@@ -1,8 +1,9 @@
 module Oxide
   class AppliedDirective
     getter name : String
+    property values : Hash(String, JSON::Any)
 
-    def initialize(@name : String, @values : Hash(String, CoercedInput) = {} of String => CoercedInput)
+    def initialize(@name : String, @values = {} of String => JSON::Any)
     end
   end
 end
