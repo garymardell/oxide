15051e69c1e4f342e7c416e08ab1a7e1af089729
Gary Mardell <info@garymardell.co.uk>
2024-03-27 18:32:42 -0700

    Skip validation of selections when part of introspection

--- src/oxide/validation/rules/field_selections.cr
+++ src/oxide/validation/rules/field_selections.cr
@@ -6,7 +6,7 @@ module Oxide
 
         case type = context.parent_type
         when Types::ObjectType, Types::InterfaceType
-          unless type.fields.has_key?(field_name)
+          unless type.fields.has_key?(field_name) || introspection_field?(type, field_name)
             context.errors << Error.new("Field '#{field_name}' doesn't exist on type '#{type.name}'")
           end
         when Types::UnionType
@@ -14,6 +14,10 @@ module Oxide
             context.errors << Error.new("Selections can't be made directly on unions (see selections on #{type.name})")
           end
         end
+      end
+
+      private def introspection_field?(type, field_name)
+        (type.name == "Query" && field_name == "__schema") || field_name == "__typename"
       end
     end
   end
