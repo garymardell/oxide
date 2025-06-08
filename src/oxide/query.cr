5baaff930dec445f2c5fd9126d4805e918de9a45
Gary Mardell <info@garymardell.co.uk>
2025-06-08 11:14:43 -0700

    Allow max tokens to be parsed with query.document

--- src/oxide/query.cr
+++ src/oxide/query.cr
@@ -14,19 +14,19 @@ module Oxide
     @[JSON::Field(ignore: true)]
     @document : Oxide::Language::Nodes::Document?
 
-    def initialize(@query_string, @variables = {} of String => JSON::Any, @operation_name = nil, @max_tokens : Int32? = nil)
+    def initialize(@query_string, @variables = {} of String => JSON::Any, @operation_name = nil)
     end
 
     def accept(visitor : Language::Visitor)
       document.accept(visitor)
     end
 
-    def document : Oxide::Language::Nodes::Document
-      @document ||= parse(query_string)
+    def document(max_tokens : Int32 = nil) : Oxide::Language::Nodes::Document
+      @document ||= parse(query_string, max_tokens)
     end
 
-    private def parse(query_string)
-      Oxide::Language::Parser.parse(query_string, @max_tokens)
+    private def parse(query_string, max_tokens)
+      Oxide::Language::Parser.parse(query_string, max_tokens)
     end
   end
 end
